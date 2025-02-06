0b767c240ad1c094767783735c6b422bacf32415
Gorm42 <161018775+Gorm42@users.noreply.github.com>
2025-02-06 12:48:29 +0100

    Oprettet klassen "Demo" og lavet fire properties. Message. Title. Description. Author.

---
+++ MyFirstGitHubClassRoomConsoleApp60/Demo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstGitHubClassRoomConsoleApp60
+{
+    internal class Demo
+    {
+
+        public string Message { get { return Message; } set { Message = value; } }
+        public string Title { get { return Title; } set { Title = value; } }
+        public string Description { get { return Description; } }
+        public string Author { get { return Author; } set { Author = value; } }
+
+
+
+    }
+}
